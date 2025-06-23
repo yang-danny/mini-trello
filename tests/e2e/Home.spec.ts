c3dddcdc531671ffe8f10242deb5e402aa47aa64
Danny <yang_danny@hotmail.com>
2025-06-23 16:25:44 +1000

    update Home.spec.ts file

--- tests/e2e/Home.spec.ts
+++ tests/e2e/Home.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 //Test task and target page
 test.describe('Home Page E2E', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173');
+    await page.goto('https://minitrello0.netlify.app');
   });
 
   
